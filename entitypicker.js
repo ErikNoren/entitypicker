c31c5bb3852057bfac6a0bb047094f75b5689f06
Erik Noren <noren.erik@gmail.com>
2013-02-27 00:26:03 -0500

    initial commit of entitypicker
    This is version 0.2.0 and depends on jQuery (I've tested with as low as
    1.7.3 - can't imagine it requires anything more than when .on() was
    introduced) and jQuery UI for the autocomplete control, styling and
    effects. Supports multiple pickers on a page, different service
    endpoints for each picker, form submission with input name pulled from
    either the id of the container or an HTML5 style data attribute, adding
    entities through javascript, querying out entities through javascript
    and more, I'm sure. Lots of customization to get UI to be unobtrusive.

---
+++ entitypicker.js
@@ -0,0 +1,240 @@
+/*
+Entity Picker v 0.2.0
+Copyright (C) 2013 Erik Noren
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+;(function ( $, undefined ) {
+	var dataKey = "entitypicker";
+	var entityAddedEvent = $.Event("entityadded");
+	var entityRemovedEvent = $.Event("entityremoved");
+	
+	var defaults = {
+		maxEntities: -1, //unlimited selection
+		minSearchLength: 2, //start search after 2 characters
+		source: function( request, response ) {
+		},
+	};
+	
+	function resolveOptions(jQelem, options) {
+		var allOptions = $.extend({}, defaults, options);
+		
+		//check for supported html5 data members and override global options
+		if (jQelem.data("max-entities")) {
+			allOptions.maxEntities = jQelem.data("max-entities");
+		}
+		
+		if (jQelem.data("min-search-length")) {
+			allOptions.minSearchLength = jQelem.data("min-search-length");
+		}
+		
+		if (jQelem.data("input-name")) {
+			allOptions.inputName = jQelem.data("input-name");
+		} else {
+			allOptions.inputName = jQelem.attr('id');
+		}
+		
+		return allOptions;
+	}
+	
+	function getEntityHtml(value, text, inputName) {
+		return "<div class='entityContainer'><div class='entityEntry ui-widget ui-widget-content ui-state-default'><div class='innerWrapper'><span data-entity-id='"
+				+ value + "' class='entityDisplay'>" + text +
+				"</span><span class='ui-icon ui-icon-close deleteEntity'></span></div></div><input name='"
+				+ inputName + "' type='hidden' value='" + value + ";" + text + "'/></div>"
+	}
+	
+	var methods = {
+	
+		addEntity: function(entity) {
+			return this.each(function() {
+				$this = $(this);
+				data = $this.data(dataKey);
+				if ( !data /*not initialized*/ ) {
+					alert("You must first configure the picker before adding entries.");
+				} else {
+					var existingUser = $this.find(".entityEntry span[data-entity-id='" + entity.value + "']");
+					if (existingUser.length > 0) {
+						existingUser.effect("highlight", {}, 1000);
+						return;
+					}
+					
+					//Add a span to the div and reposition the div under the searh text box and reset the controls
+					ctl = $this.find(".entityPickerParent div:first");
+					newEntity = $(getEntityHtml(entity.value, entity.text, data.inputName));
+					ctl.append(newEntity);
+					entityAddedEvent.value = newEntity.find("input").val();
+					entityAddedEvent.text = entity.text;
+					entityAddedEvent.inputName = data.inputName;
+					$this.trigger(entityAddedEvent);
+
+					maxEntities = data.maxEntities;
+					if (maxEntities >= 0) {
+						if (ctl.find(".entityContainer").length >= maxEntities) {
+							$this.addClass("entityPickerDisabled");
+							$this.find("input.entityPickerInput").hide();
+						}
+					}
+				}
+			});
+		},
+		
+		addEntities: function(entities) {
+			return this.each(function() {
+				$this = $(this);
+				data = $this.data(dataKey);
+				
+				if ( !data /*not initialized*/ ) {
+					alert("You must first configure the picker before adding entries.");
+				} else {
+					userContainer = $this.find(".entityPickerParent div:first");
+					
+					$.each(entities, function() {
+						userContainer.append(getEntityHtml(this.value, this.text, data.inputName));
+					});
+					
+					var maxEntities = data.maxEntities;
+					if (maxEntities >= 0) {
+						if ($this.find(".entityContainer").length >= maxEntities) {
+							$this.addClass("entityPickerDisabled");
+							$this.find("input.entityPickerInput").hide();
+						}
+					}
+				}
+			});
+		},
+		
+		getEntities: function(entities) {
+			if (typeof entities === 'undefined') {
+				entities = new Array();
+			}
+			
+			$(this).find(".entityContainer input").each(function() {
+				entities.push({ value: this.value, name: this.name });
+			});
+			
+			return entities;
+		},
+		
+		init: function(options) {
+			return this.each(function() {
+				$this = $(this);
+				data = $this.data(dataKey);
+				
+				if ( ! data ) {
+					configured = resolveOptions($this, options);
+					$this.data(dataKey, configured);
+					
+					//Set up the main HTML container:
+					$this.html("<div class='entityPickerParent'><div></div><input type='text' class='entityPickerInput ui-widget'/><div class='entityClear'></div></div>");
+					
+					if (configured.maxEntities > 0) {
+						$this.attr("title", "This field is limited to " + configured.maxEntities + " selection" + (configured.maxEntities > 1 ? "s." : "."));
+					}
+					
+					$this.on("click", "span.deleteEntity", function () {
+						var ctl = $(this).closest(".entityContainer");
+						entityContainer = ctl.find("input");
+						entityRemovedEvent.value = entityContainer.val();
+						entityRemovedEvent.inputName = entityContainer.attr("name");
+						$this.trigger(entityRemovedEvent);
+						ctl.remove();
+						
+						var maxEntities = configured.maxEntities;
+						var currentEntityCount = $this.find(".entityContainer").length;
+						if (maxEntities < 0 || maxEntities > currentEntityCount) {
+							$this.removeClass("entityPickerDisabled");
+							$this.find("input.entityPickerInput").show().focus();
+						}
+					});
+					
+					$this.on("click", ":not(span.deleteEntity)", function (event) {
+						var that = $(this).find("input.entityPickerInput");
+						that.focus().val(that.val());
+						event.stopPropagation();
+					});
+					
+					pickerInput = $this.find("input.entityPickerInput");
+					pickerInput.autocomplete({
+						source: configured.source,
+						minLength: configured.minSearchLength,
+						select: function( event, ui ) {
+							if ( ui.item ) {
+								pickerContainer = $this.parent(".entityPickerParent").parent("div");
+								methods.addEntity.call(pickerContainer, {value: ui.item.value, text: ui.item.label});
+								this.value = "";
+								this.focus();
+								return false;
+							}
+						},
+						focus: function( event, ui ) {
+							//prevent the autocomplete's input from changing as the user
+							//navigates the available options. This seems like a better
+							//user experience when data isn't a simple list.
+							this.selectedItem = null;
+							return false;
+						},
+						open: function() {
+							$( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+						},
+						close: function() {
+							$( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+						}
+					});
+					
+					pickerInput.on("keydown", function(event) {
+						$this = $(this);
+						if (event.keyCode == $.ui.keyCode.BACKSPACE) {
+							if ($this.val().length < 1) {		
+								entityContainer = $this.parent().find(".entityContainer:last");
+								entityInput = entityContainer.find("input");
+								entityRemovedEvent.value = entityInput.val();
+								entityRemovedEvent.inputName = entityInput.attr("name");
+								$this.trigger(entityRemovedEvent);
+								entityContainer.remove();
+							}
+						}
+					});
+					
+					if (configured.maxEntities >= 0) {
+						if ($this.find(".entityContainer").length >= configured.maxEntities) {
+							$this.addClass("entityPickerDisabled");
+							$this.find("input.entityPickerInput").hide();
+						}
+					}
+				}
+			});
+		}
+	};
+	
+	$.fn.entitypicker = function(method) {	
+		if (typeof $.fn.autocomplete === 'undefined') {
+			alert("This component requires jQuery autocomplete.");
+			return this;
+		}
+		
+		if (typeof $.fn.effect === 'undefined') {
+			alert("This component requries jQuery UI effect.");
+			return this;
+		}
+		
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		}
+	};
+})( jQuery );
